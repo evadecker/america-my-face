80e6f40b1c14db52f87283483db813474dbce888
Eva Decker <itsevadecker@gmail.com>
2024-11-07 19:32:32 -0500

    gitignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/static";
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
-  plugins: ["prettier-plugin-astro"],
+  adapter: netlify(),
+  plugins: ["prettier-plugin-astro"]
 });
